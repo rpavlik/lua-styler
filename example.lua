c33aca9b09f091123a88d3ac10d53b3aa2b8f58c
Ryan Pavlik <rpavlik@iastate.edu>
2012-02-06 18:00:08 -0600

    make the example more extensive

--- example.lua
+++ example.lua
@@ -5,3 +5,19 @@ print "Whoopie!"
 else
  print "noooo"
  end
+ 
+local z = function() print "hats" end
+function morehats()
+print "lots and lots"
+		print "of hats"
+		end
+		
+
+local y = {
+	["a"] = 5,
+b = 6,
+c = 8,
+d = function()
+	print "function in a table!"
+	end,
+}
