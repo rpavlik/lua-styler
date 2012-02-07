73fb6622ec11374baf134bad504776286b0d0ba3
Ryan Pavlik <rpavlik@iastate.edu>
2012-02-07 14:03:57 -0600

    Make a win32 folder with everything needed to run.

---
+++ win32/lua-styler-nobackups.cmd
@@ -0,0 +1,8 @@
+@echo off
+SETLOCAL
+
+set SCRIPTDIR=%~dp0
+set LUA_PATH=%SCRIPTDIR%lua/?.lua;%SCRIPTDIR%lua/?/init.lua;;
+set LUA_CPATH=%SCRIPTDIR%lib/?.dll;;
+"%SCRIPTDIR%lua-styler.lua" --nobackup %*
+pause
